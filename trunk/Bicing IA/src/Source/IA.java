0fddec48c26cb5ca10b737a25ac8369c8dac755f
black.mask.dani@gmail.com <black.mask.dani@gmail.com@a83a6749-ce44-6f97-ee03-d58235f6bcea>
2012-03-08 17:28:07 +0000

    DMC: Added initial files of the project.

---
+++ trunk/Bicing IA/src/Source/IA.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Source;
+
+import Source.State;
+
+/**
+ *
+ * @author Dani
+ */
+public class IA {
+
+
+    
+    // Pre: assuming ther're enough bicycles to cover the expected amount of bycycles
+    public State GetInitialState() {
+        
+        return null;
+        
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
