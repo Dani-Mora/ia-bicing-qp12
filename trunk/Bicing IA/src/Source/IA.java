179520192b57d6caf120d28e75984d936e39b964
black.mask.dani@gmail.com <black.mask.dani@gmail.com@a83a6749-ce44-6f97-ee03-d58235f6bcea>
2012-03-08 18:23:18 +0000

    DMC: Added util functions and updated main class and state.

--- trunk/Bicing IA/src/Source/IA.java
+++ trunk/Bicing IA/src/Source/IA.java
@@ -4,27 +4,47 @@
  */
 package Source;
 
-import Source.State;
+import IA.Bicing.Bicing;
+import IA.Bicing.test;
+import java.util.Random;
 
 /**
  *
  * @author Dani
  */
 public class IA {
-
-
+    
+    private Bicing context;
+    private static final Integer NUM_VANS = 6;
+    
+    private Random myRandom;
+    
+    public IA() {
+        this.context = new Bicing(20,120, 0);    
+    }
+    
     
     // Pre: assuming ther're enough bicycles to cover the expected amount of bycycles
     public State GetInitialState() {
+        Integer[] numBic = new Integer[this.context.getNumStations()];        
+        //Integer[] sortedStations = new Integer[this.context.getNumStations()];
+        Integer[] vansStation = new Integer[NUM_VANS]; 
         
-        return null;
-        
+        int numStations = this.context.getNumStations();
+        for (int i = 0; i < numStations; ++i) {
+            // Stations
+            numBic[i] = this.context.getStationNextState((i));
+            vansStation[i] = this.myRandom.nextInt(numStations);
+            // Vans
+        }
+     
+        return new State(numBic, vansStation);       
     }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         test a = new test();       
     }
 }
